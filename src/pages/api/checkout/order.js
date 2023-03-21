f56f72986ff9e3283a000599f028bccd5df7b8eb
Arun9650 <arun02580@gmail.com>
2023-03-21 13:32:36 +0530

    try to fix the address genertor

--- src/pages/api/checkout/order.js
+++ src/pages/api/checkout/order.js
@@ -1,6 +1,15 @@
 const api_key = "3Rq4mmTlEt7eCy8R4vw7lYarXNaFj4CKtqWqExNEtOA";
 import axios from "axios";
 async function order(req, res) {
+
+
+    const status = req.query.status || -1;
+    const address = req.query.addr || 'na';
+    const amount = (req.query.value || 0) / 1e8;
+    const txid = req.query.txid || 'na';
+
+
+
   const addressResponse = await axios({
     method: "post",
     url: "https://www.blockonomics.co/api/new_address?reset=1",
@@ -15,10 +24,10 @@ async function order(req, res) {
   );
 
   console.log(response.data.price);
-  const address = addressResponse.data.address;
-  console.log(address);
+  const addressbtc = addressResponse.data.address;
+  console.log(addressbtc);
 
-  const data = { address, price: response.data.price };
+  const data = { addressbtc, price: response.data.price };
 
   console.log(data);
 
